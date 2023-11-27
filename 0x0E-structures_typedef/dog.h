e0713e04fc5eefb4da5319878eb2cfd4fd95656e
Smolclin <clinsmol10@gmail.com>
2023-11-27 13:18:15 -0800

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - function that creates dog
+ * info
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: my first alx struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
