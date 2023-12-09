871fba701706873e72566c17976bef094aaf8f79
Smolclin <clinsmol10@gmail.com>
2023-12-09 04:14:23 -0800

    Correction

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-/**
- * myFunction - applies the constructor
- */
-
 void myFunction(void) __attribute__ ((constructor));
-/*
- * myFunction - function that prints when
- * executed
+/**
+ * myFunction - function that prints when executed
  */
 
 void myFunction(void)
+
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
