664134421f58182005e7b9ec614311c88c208f04
Smolclin <clinsmol10@gmail.com>
2023-12-09 04:43:59 -0800

    print list correction

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,20 +12,18 @@
 
 size_t print_list(const list_t *h)
 {
-	int j = 0;
+	int j;
 
-	while (h)
+	if (h == NULL)
+		return (0);
+	for (j = 1; h->next != NULL; j++)
 	{
 		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
+			printf("[%u] %s\n", h->len, "(nil)");
 		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
-		j++;
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 	}
-	return (j++);
+	printf("[%u] %s\n", h->len, h->str);
+	return (j);
 }
