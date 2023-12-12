513aee7bf055271e6a24039f2eed2917f97a309d
Smolclin <clinsmol10@gmail.com>
2023-12-11 23:43:13 -0800

    correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,15 +9,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t cnt;
+	size_t count;
 
 	if (h == NULL)
 		return (0);
 
-	for (cnt = 0; h != NULL; cnt++)
+	for (count = 0; h != NULL; count++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return (cnt);
+	return (count);
 }
